2c532cbc4f08902c32a1474bf18631e9e0521ee7
VijaySubramaniam <VijaySubramaniam@DESKTOP-DT7RJ3F>
2020-01-03 13:54:59 -0600

    Interface - University and Class Implementation - UnivImpl

---
+++ Java_All_The_Way/src/org/gratis/ocap/UnivImpl.java
@@ -0,0 +1,9 @@
+package org.gratis.ocap;
+
+public class UnivImpl implements University {
+
+	public void UnivState() {
+		System.out.println("Texas");
+	}
+
+}
---
+++ Java_All_The_Way/src/org/gratis/ocap/University.java
@@ -0,0 +1,9 @@
+package org.gratis.ocap;
+
+public interface University {
+
+	public void UnivState();
+	static String UnivName() {
+		return "UHCL";
+	}
+}
